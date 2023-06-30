5a55e990ac9b733321ab0275a2b3da413cff9466
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-30 18:56:09 +0600

    trpc setup in pages route

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import { trpc } from "@/utils/trpc";
+
+export default function IndexPage() {
+  // 💡 Tip: CMD+Click (or CTRL+Click) on `greeting` to go to the server definition
+  const result = trpc.greeting.useQuery({ name: "clients" });
+
+  if (!result.data) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex justify-center items-center">
+      <h1>{result.data.text}</h1>
+    </div>
+  );
+}
--- utils/trpc.ts
+++ utils/trpc.ts
@@ -1,29 +1,13 @@
-import { AppRouter } from "@/app/api/trpc/[trpc]";
+import { AppRouter } from "@/pages/api/trpc/[trpc]";
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
-
-function getBaseUrl() {
-  if (typeof window !== "undefined") {
-    // In the browser, we return a relative URL
-    return "";
-  }
-  // When rendering on the server, we return an absolute URL
-
-  // reference for vercel.com
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
-
-  // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-}
 
 export const trpc = createTRPCNext<AppRouter>({
   config() {
     return {
       links: [
         httpBatchLink({
-          url: getBaseUrl() + "/api/trpc",
+          url: "http://localhost:3000/api/trpc",
         }),
       ],
     };
